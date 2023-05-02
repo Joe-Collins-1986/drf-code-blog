750f155664a4ec51483abf6541ab4f3192a76f1c
Joseph Collins <joecollins@Josephs-MBP.home>
2023-05-02 16:58:26 +0100

    replace article list and detail with generics

--- articles/views.py
+++ articles/views.py
@@ -1,76 +1,17 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Article
 from .serializers import ArticleSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
 
-class ArticleList(APIView):
+class ArticleList(generics.ListCreateAPIView):
     serializer_class = ArticleSerializer
-    permission_classes = [
-            permissions.IsAuthenticatedOrReadOnly
-        ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Article.objects.all()
 
-    def get(self, request):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(
-            articles,
-            many=True,
-            context={
-                'request': request
-                })
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = ArticleSerializer(
-            data=request.data,
-            context={
-                'request': request
-                })
-        
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
-    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-class ArticleDetail(APIView):
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ArticleSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    def get_object(self, pk):
-        try:
-            article = Article.objects.get(pk=pk)
-            return article
-        except Article.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        article = self.get_object(pk)
-        serializer = ArticleSerializer(
-            article,
-            context={
-                'request': request
-                })
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        article = self.get_object(pk)
-        serializer = ArticleSerializer(
-            article, data=request.data,
-            context={
-                'request': request
-                })
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        article = self.get_object(pk)
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Article.objects.all()
